35c4a586ec2c4be63493e0ff3bb4557031a2e403
Laercio Rios <laercio.rios13@hotmail.com>
2020-01-04 10:16:39 -0300

    Iniciado front-end

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,97 +4,68 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Desafio GolFarma</title>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/app_style.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <div class="container content-tasknow">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <br/>
+                    <br/>
+                    <h1>Médicos</h1>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+
+                    <table class="table table-light">
+                        <thead class="thead-light">
+                        <tr>
+                            <th scope="col" colspan="2">Nome</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {{--
+                        @if(count($tiposTarefasUser) == 0)
+                            <tr>
+                                <td>Não existe médico cadastrados</td>
+                            </tr>
+                        @else
+                            @foreach($tiposTarefasUser as $tipoTarefa)
+                                <tr>
+                                    <td>{{$tipoTarefa->descricao}}</td>
+                                    <td>
+                                        <form action = "{{route('tipotarefa.destroy', $tipoTarefa->id)}}" method = "POST">
+                                            @csrf
+                                            <a class = "btn" href="{{route('tipotarefa.edit', $tipoTarefa->id)}}"><img src="{{ asset('imagens/edit.png') }}" title="Editar"></a>
+                                            @method('DELETE')
+                                            <button type = "submit" class = "btn btn-link"><img src="{{ asset('imagens/delete.png') }}" title="Excluir"></button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
                         @endif
-                    @endauth
-                </div>
-            @endif
+                        php>
+                        --}}
+                        </tbody>
+                    </table>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
+
+        <a class = "btn" href=""><img src="{{ asset('imagens/mais.png') }}" class="button-mais"/></a>
     </body>
 </html>
